cb1ee9d585648c9537726aa84ca5bb8ffa747eaa
vladi <vladislav_fedotov_official@bk.ru>
2024-01-15 16:32:09 +0300

    рефакторинг

--- databaseAPI/src/main/kotlin/com/raif/databaseapi/web/controller/impl/PaymentControllerImpl.kt
+++ databaseAPI/src/main/kotlin/com/raif/databaseapi/web/controller/impl/PaymentControllerImpl.kt
@@ -4,14 +4,7 @@ import com.raif.databaseapi.domain.PaymentInfo
 import com.raif.databaseapi.service.PaymentService
 import com.raif.databaseapi.web.controller.PaymentController
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("database-api/v1/payments")
@@ -24,9 +17,9 @@ class PaymentControllerImpl(
         return ResponseEntity.ok(null)
     }
 
-    @PutMapping
-    override fun updatePaymentInfo(@RequestParam("qr_id") qrId: String,
-                                   @RequestParam("payment_status") paymentStatus: String): ResponseEntity<*> {
+    @PutMapping("/{qrId}")
+    override fun updatePaymentInfo(@PathVariable("qrId") qrId: String,
+                                   @RequestBody paymentStatus: String): ResponseEntity<*> {
         paymentService.updatePaymentInfo(qrId, paymentStatus)
         return ResponseEntity.ok(null)
     }
