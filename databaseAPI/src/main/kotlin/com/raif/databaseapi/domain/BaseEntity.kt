839b6b2ba7ff854fca7a8e8f07106a3239b12e59
vladi <vladislav_fedotov_official@bk.ru>
2024-01-15 15:55:22 +0300

    рефакторинг

--- databaseAPI/src/main/kotlin/com/raif/databaseapi/domain/BaseEntity.kt
+++ databaseAPI/src/main/kotlin/com/raif/databaseapi/domain/BaseEntity.kt
@@ -4,11 +4,13 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.MappedSuperclass
+import jakarta.validation.constraints.Null
 
 @MappedSuperclass
 abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Null
     var id: Long? = null
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
