f1fc23db16182c6824160d6889e09a586dc8c118
Nikita Efimov <nikita-efimov04@mail.ru>
2024-01-28 21:28:03 +0300

    change payment-api and database-api links

--- botConstructors/src/main/kotlin/com/raif/botConstructors/services/impl/QRCodeServiceImpl.kt
+++ botConstructors/src/main/kotlin/com/raif/botConstructors/services/impl/QRCodeServiceImpl.kt
@@ -11,8 +11,7 @@ import java.util.*
 class QRCodeServiceImpl(private val restTemplate: RestTemplate) : QRCodeService {
     override fun getQR(amount: Double): QR {
         val url = "http://147.78.66.234:8081/payment-api/v1/qrs/dynamic"
-//        val url = "http://host.docker.internal:8081/payment-api/v1/qrs/dynamic"
-        val order = UUID.randomUUID().toString();
+        val order = UUID.randomUUID().toString()
         val request = mapOf("amount" to amount, "order" to order)
         val response: ResponseEntity<QR> = restTemplate.postForEntity(url, request, QR::class.java)
 
@@ -26,7 +25,6 @@ class QRCodeServiceImpl(private val restTemplate: RestTemplate) : QRCodeService 
 
     override fun updateQR(qr: QR): QR {
         val url = "http://147.78.66.234:9091/database-api/v1/qrs/"
-//        val url = "http://host.docker.internal:9091/database-api/v1/qrs/"
         val response = restTemplate.getForObject(url + qr.qrId, QR::class.java)
         return response ?: throw Exception("Failed to get QR")
 
