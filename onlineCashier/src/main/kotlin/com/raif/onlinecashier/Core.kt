ab7517c3dbbbb874d87afb54c0cf9ea30975e0a7
Saprykin Maxim <saprykinoff@inbox.ru>
2024-01-29 21:57:32 +0300

    Move build QrObject from json to separate function. Same with receiving Qr by id

--- onlineCashier/src/main/kotlin/com/raif/onlinecashier/Core.kt
+++ onlineCashier/src/main/kotlin/com/raif/onlinecashier/Core.kt
@@ -1,7 +1,18 @@
 package com.raif.onlinecashier
 
 import org.json.JSONException
-import java.util.UUID;
+import org.json.JSONObject
+import java.util.UUID
+
+
+fun QrFromJson(json: JSONObject): QrObject {
+    return QrObject(
+        json["qrId"].toString(),
+        json["qrUrl"].toString(),
+        json["payload"].toString(),
+        json["qrStatus"].toString()
+    )
+}
 
 
 fun generateQr(price: Double, marketId: String): QrObject? {
@@ -21,35 +32,38 @@ fun generateQr(price: Double, marketId: String): QrObject? {
         //  записывать в базу (qrid, qrurl, qrstatus, marketid, price)
         //  создавать поток который следит за оплатой заказа
 
-        return QrObject(
-            resp["qrId"].toString(),
-            resp["qrUrl"].toString(),
-            resp["qrStatus"].toString()
-        )
+        return QrFromJson(resp)
     } catch (e: JSONException) {
         return null
     }
 }
 
+fun getQrById(qrId: String): QrObject? {
+    println("Send get qr=$qrId request")
+    val response = khttp.get(
+        "http://147.78.66.234:9091/database-api/v1/qrs/$qrId"
+    )
+    println("Response received")
+    return try {
+        QrFromJson(response.jsonObject)
+    } catch (_: Exception) {
+        null
+    }
 
+}
 
 
 fun checkPayment(marketId: String, qrId: String, replyTo: Int) {
     val mybot = MyBot()
     while (true) {
-        println("Send qr post request")
-        val response = khttp.get(
-            "http://147.78.66.234:9091/database-api/v1/qrs/$qrId"
-        )
-        println("Response received")
-
-        try {
-            response.jsonObject
-        } catch (_: Exception) {
+        val qr = getQrById(qrId)
+        if (qr == null) {
             mybot.sendMessageExecute(marketId, "Ошибка при обновлении статуса qr", replyTo = replyTo)
+            break
         }
-        val resp = response.jsonObject
-        val qrStatus = resp["qrStatus"]
+        println(qr.qrStatus)
+
+        val qrStatus = qr.qrStatus
         when (qrStatus) {
             "PAID" -> {
                 mybot.sendMessageExecute(marketId, "Qr успешно оплачен", replyTo = replyTo)
--- onlineCashier/src/main/kotlin/com/raif/onlinecashier/Data.kt
+++ onlineCashier/src/main/kotlin/com/raif/onlinecashier/Data.kt
@@ -1,8 +1,10 @@
 package com.raif.onlinecashier
 
+
 data class QrObject(
     val qrId: String,
     val qrUrl: String,
+    val payload: String,
     val qrStatus: String,
 )
 
