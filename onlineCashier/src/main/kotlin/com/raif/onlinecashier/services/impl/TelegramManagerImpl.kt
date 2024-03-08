059b81a4747d6c00b26533cc13804b04a9658139
Saprykin Maxim <saprykinoff@inbox.ru>
2024-03-08 13:40:11 +0300

    Made cart, commands to interact, data layer.

--- onlineCashier/src/main/kotlin/com/raif/onlinecashier/services/impl/TelegramManagerImpl.kt
+++ onlineCashier/src/main/kotlin/com/raif/onlinecashier/services/impl/TelegramManagerImpl.kt
@@ -1,5 +1,6 @@
 package com.raif.onlinecashier.services.impl
 
+import com.raif.onlinecashier.services.DataService
 import com.raif.onlinecashier.services.TelegramService
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Service
@@ -7,23 +8,55 @@ import org.telegram.telegrambots.meta.api.objects.Update
 
 @Service
 class TelegramManagerImpl(
-    private var telegramService: TelegramService
+    private var telegramService: TelegramService,
+    private var dataService: DataService
 ) {
 
     @EventListener
     fun update(update: Update) {
         if (update.hasMessage()) {
             val msg = update.message
-            if (msg.text.startsWith("/add")) {
-                val splt = msg.text.split(" ")
-                add(msg.chatId, splt[1], splt[2].toDouble())
+            val chatId = msg.chatId
+            val text = msg.text
+
+            if (text.startsWith("/addToMenu")) {
+                val splt = text.split(" ")
+                addToMenu(chatId, splt[1], splt[2].toDouble())
+            } else if (text.startsWith("/addToOrder")) {
+                val splt = text.split(" ")
+                addToOrder(chatId, splt[1])
+            } else if (text == "/listMenu") {
+                getMenu(chatId)
+            } else if (text == "/listOrder") {
+                getOrder(chatId)
             }
         }
     }
 
-
-
-    private fun add(chatId: Long, name: String, price: Double) {
+    private fun addToMenu(chatId: Long, name: String, price: Double) {
         telegramService.sendMessage(chatId, "add $name: $price")
+        dataService.addMenuProduct(chatId, name, price)
+    }
+
+    private fun getMenu(chatId: Long) {
+        val menu = dataService.listMenu(chatId)
+        telegramService.sendMessage(chatId, menu.toString())
+    }
+
+    private fun getOrder(chatId: Long) {
+        val order = dataService.listOrder(chatId)
+        telegramService.sendMessage(chatId, order.toString())
+    }
+
+    private fun addToOrder(chatId: Long, name: String) {
+        val menu = dataService.listMenu(chatId)
+        if (!menu.contains(name)) {
+            telegramService.sendMessage(chatId, "Такого товара нет в меню")
+            return
+        }
+        dataService.addOrderProduct(chatId, name)
+        telegramService.sendMessage(chatId, "Товар успешно добавлен в заказ" +
+                "")
+
     }
 }
