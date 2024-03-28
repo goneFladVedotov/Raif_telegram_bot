6db2a1f5c7fa4f0549eb2cad9e29ddc4d1004fa8
vladi <vladislav_fedotov_official@bk.ru>
2024-03-28 18:11:29 +0300

    добавил документацию

--- databaseAPI/src/main/kotlin/com/raif/databaseapi/web/controller/impl/QrControllerImpl.kt
+++ databaseAPI/src/main/kotlin/com/raif/databaseapi/web/controller/impl/QrControllerImpl.kt
@@ -5,15 +5,19 @@ import com.raif.databaseapi.mapper.Mapper
 import com.raif.databaseapi.service.QrService
 import com.raif.databaseapi.web.controller.QrController
 import com.raif.databaseapi.web.dto.QrInfoDto
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("database-api/v1/qrs")
+@Tag(name = "Qr Controller")
 class QrControllerImpl(
     private val qrService: QrService,
     private val mapper: Mapper<QrInfo, QrInfoDto>
-): QrController {
+) : QrController {
+    @Operation(summary = "Сохранение QR")
     @PostMapping
     override fun saveQrInfo(@RequestBody qrInfoDto: QrInfoDto): ResponseEntity<*> {
         val qrInfo = mapper.dtoToEntity(qrInfoDto)
@@ -21,18 +25,21 @@ class QrControllerImpl(
         return ResponseEntity.ok(null)
     }
 
+    @Operation(summary = "Получение информации о QR")
     @GetMapping("/{qrId}")
     override fun getQrInfo(@PathVariable("qrId") qrId: String): ResponseEntity<QrInfoDto> {
         val qrInfoDto = mapper.entityToDto(qrService.getQrInfo(qrId))
         return ResponseEntity.ok(qrInfoDto)
     }
 
+    @Operation(summary = "Получение всех QR")
     @GetMapping
     override fun getAll(): ResponseEntity<List<QrInfoDto>> {
         return ResponseEntity.ok(mapper.entityToDto(qrService.getAll()))
     }
 
-    @PutMapping("/{qrId}")
+    @Operation(summary = "Обновление статуса QR")
+    @PatchMapping("/{qrId}")
     override fun updateQrStatus(@PathVariable("qrId") qrId: String, @RequestBody qrStatus: String): ResponseEntity<*> {
         qrService.updateQrInfo(qrId, qrStatus)
         return ResponseEntity.ok(null)
--- databaseAPI/src/main/kotlin/com/raif/databaseapi/web/controller/impl/RefundControllerImpl.kt
+++ databaseAPI/src/main/kotlin/com/raif/databaseapi/web/controller/impl/RefundControllerImpl.kt
@@ -5,15 +5,19 @@ import com.raif.databaseapi.mapper.Mapper
 import com.raif.databaseapi.service.RefundService
 import com.raif.databaseapi.web.controller.RefundController
 import com.raif.databaseapi.web.dto.RefundInfoDto
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/database-api/v1/refund")
+@Tag(name = "Refund Controller")
 class RefundControllerImpl(
     private val refundService: RefundService,
     private val mapper: Mapper<RefundInfo, RefundInfoDto>
-): RefundController {
+) : RefundController {
+    @Operation(summary = "Сохранение возврата")
     @PostMapping
     override fun saveRefundInfo(@RequestBody refundInfoDto: RefundInfoDto): ResponseEntity<*> {
         val refundInfo = mapper.dtoToEntity(refundInfoDto)
@@ -21,19 +25,24 @@ class RefundControllerImpl(
         return ResponseEntity.ok(null)
     }
 
-    @PutMapping("/{refundId}")
-    override fun updateRefundStatus(@PathVariable("refundId") refundId: String,
-                                    @RequestBody refundStatus: String): ResponseEntity<*> {
+    @Operation(summary = "Обновление статуса возврата")
+    @PatchMapping("/{refundId}")
+    override fun updateRefundStatus(
+        @PathVariable("refundId") refundId: String,
+        @RequestBody refundStatus: String
+    ): ResponseEntity<*> {
         refundService.updateRefundStatus(refundId, refundStatus)
         return ResponseEntity.ok(null)
     }
 
+    @Operation(summary = "Получение информации о возврате")
     @GetMapping("/{refundId}")
     override fun getRefundInfo(@PathVariable("refundId") refundId: String): ResponseEntity<RefundInfoDto> {
         val refundInfoDto = mapper.entityToDto(refundService.getRefundInfo(refundId))
         return ResponseEntity.ok(refundInfoDto)
     }
 
+    @Operation(summary = "Получение все возвратов")
     @GetMapping
     override fun getAll(): ResponseEntity<List<RefundInfoDto>> {
         return ResponseEntity.ok(mapper.entityToDto(refundService.getAll()))
