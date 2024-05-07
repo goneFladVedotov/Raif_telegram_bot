aeca6efa301f56b293668e2ef6050a17054869e5
Nikita Efimov <nikita-efimov04@mail.ru>
2024-05-07 09:01:08 +0300

    refactor smartbotpro service

--- botConstructors/src/main/kotlin/com/raif/botConstructors/services/SmartBotProService.kt
+++ botConstructors/src/main/kotlin/com/raif/botConstructors/services/SmartBotProService.kt
@@ -4,6 +4,10 @@ import com.raif.botConstructors.models.Order
 import com.raif.botConstructors.models.dto.RefundDto
 
 interface SmartBotProService {
+
+    fun orderMessage(order: Order, message: String)
     fun orderPaid(order: Order)
     fun orderRefund(order: Order, refundDto: RefundDto)
+    fun orderExpired(order: Order)
+
 }
--- botConstructors/src/main/kotlin/com/raif/botConstructors/services/impl/SmartBotProServiceImpl.kt
+++ botConstructors/src/main/kotlin/com/raif/botConstructors/services/impl/SmartBotProServiceImpl.kt
@@ -11,6 +11,7 @@ import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.web.client.RestTemplate
+import kotlin.math.log
 import kotlin.reflect.full.memberProperties
 
 @Service
@@ -19,13 +20,15 @@ class SmartBotProServiceImpl(
 ): SmartBotProService {
 
     private val logger = LoggerFactory.getLogger(javaClass)
-    override fun orderPaid(order: Order) {
-        logger.info("Smartbotpro order ${order.id} was paid")
+
+    override fun orderMessage(order: Order, message: String) {
+        logger.info("Smartbotpro message ${order.id} ${message}")
         val usernameProperty = order.client.extraInfo::class.memberProperties.find { it.name == "username" }
         if (usernameProperty == null) {
             throw Exception("client does not have username")
         }
         val username: String = usernameProperty.getter.call(order.client.extraInfo) as String
+        logger.info("username=${username}}")
         val headers = HttpHeaders()
         headers.contentType = MediaType.APPLICATION_JSON
         val webHookDto = SmartBotProWebHookDto(
@@ -36,7 +39,7 @@ class SmartBotProServiceImpl(
             peer_id = username,
             data = SmartBotProWebHookDto.Data(
                 order = order.id,
-                info = "Заказ успешно оплачен"
+                info = message
             )
         )
         val entity = HttpEntity(webHookDto, headers)
@@ -47,31 +50,18 @@ class SmartBotProServiceImpl(
             String::class.java
         )
     }
+    override fun orderPaid(order: Order) {
+        logger.info("Smartbotpro order ${order.id} was paid")
+        orderMessage(order, "Заказ успешно оплачен")
+    }
     override fun orderRefund(order: Order, refundDto: RefundDto) {
-        val usernameProperty = order.client.extraInfo::class.memberProperties.find { it.name == "username" }
-        if (usernameProperty == null) {
-            throw Exception("client does not have username")
-        }
-        val username: String = usernameProperty.getter.call(order.client.extraInfo) as String
-        val headers = HttpHeaders()
-        headers.contentType = MediaType.APPLICATION_JSON
-        val webHookDto = SmartBotProWebHookDto(
-            access_token = "KxstcpiIzBNMK8Wb3tV8xei2A9Wuie3XNSholMBtidYj5EvvP4FIeYl_5pkRM-ym",
-            v = "0.0.1",
-            channel_id = "6791683941",
-            block_id = "e22b977e3cc64419735c8e3d",
-            peer_id = username,
-            data = SmartBotProWebHookDto.Data(
-                order = order.id,
-                info = "Оформлен возврат на сумму ${refundDto.amount} с информацией ${refundDto.paymentDetails}"
-            )
-        )
-        val entity = HttpEntity(webHookDto, headers)
 
-        restTemplate.postForEntity(
-            "https://api.smartbotpro.ru/blocks/execute",
-            entity,
-            String::class.java
-        )
+        logger.info("Smartbotpro order ${order.id} refund")
+        orderMessage(order, "Оформлен возврат на сумму ${refundDto.amount} с информацией ${refundDto.paymentDetails}")
+    }
+
+    override fun orderExpired(order: Order) {
+        logger.info("Smartbotpro order ${order.id} expired")
+        orderMessage(order, "Срок действия заказа истек")
     }
 }
